a8ea2000ad08fbba5106467745ee3418923071a5
Saswankar Bura Gohain <111171338+Saswankar1@users.noreply.github.com>
2024-07-01 19:23:12 +0530

    Create greatesOf3.cpp

---
+++ greatesOf3.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a , b, c;
+    cin >> a,b,c;
+    if (a > b){
+        if (a>c)
+        {
+            cout << "a is the greates";
+        } else {
+            cout << "c is the greatest";
+        }
+    } else 
+    {
+        if (b > c)
+        {
+            cout << "b is the greatest";
+        } else {
+            cout << "c is the greatest";
+        }
+    }
+    
+    return 0;
+}
